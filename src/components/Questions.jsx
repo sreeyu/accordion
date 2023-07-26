3f837ef34188fe99021841e64dd3dda594aea1dd
sreey <sreelakshmi12898@gmail.com>
2023-07-26 22:46:36 +0530

    Style Questions component

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -4,8 +4,8 @@ import styles from './Questions.module.css';
 function Questions(props){
 
     return(
-        <li>
-            <div>
+        <li className={styles.questions}>
+            <div className={styles.title} >
                 <h3>{props.qs}</h3>
                 <button>+</button>
             </div>
